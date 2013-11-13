d81ddd94a9b5bef78a7fe966ef807564cc79c44d
yinonavraham@gmail.com <yinonavraham@gmail.com@9460df96-c777-8588-7e92-d0db99ffd5a7>
2013-11-13 21:17:40 +0000

    Add "set" methods for multiple updates in one call

--- trunk/ynn.util.collections/src/ynn/util/collections/sql/UpdatableCollection.java
+++ trunk/ynn.util.collections/src/ynn/util/collections/sql/UpdatableCollection.java
@@ -1,9 +1,21 @@
 package ynn.util.collections.sql;
 
+import java.util.Collection;
+
 public interface UpdatableCollection<E> extends 
 	SqlLikeCollection<E>,
 	WhereSupport<E, UpdatableCollection<E>> {
 	
 	int set(Updater<E> updater);
+	
+	int set(Updater<E> updater1, Updater<E> updater2);
+	
+	int set(Updater<E> updater1, Updater<E> updater2, Updater<E> updater3);
+	
+	int set(Updater<E> updater1, Updater<E> updater2, Updater<E> updater3, Updater<E> updater4);
+	
+	int set(Updater<E> updater1, Updater<E> updater2, Updater<E> updater3, Updater<E> updater4, Updater<E> updater5);
+	
+	int set(Collection<Updater<E>> updaters);
 
 }
