d8388767083df60a56e38af7dc48a99aefbd86ef
yinonavraham@gmail.com <yinonavraham@gmail.com@9460df96-c777-8588-7e92-d0db99ffd5a7>
2013-11-13 21:17:40 +0000

    Add "set" methods for multiple updates in one call

--- ynn.util.collections/src/ynn/util/collections/sql/UpdatableArrayList.java
+++ ynn.util.collections/src/ynn/util/collections/sql/UpdatableArrayList.java
@@ -1,7 +1,9 @@
 package ynn.util.collections.sql;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 class UpdatableArrayList<E> extends ArrayList<E> implements UpdatableCollection<E> {
 
@@ -27,17 +29,67 @@ class UpdatableArrayList<E> extends ArrayList<E> implements UpdatableCollection<
 	public UpdatableArrayList(int initialCapacity) {
 		super(initialCapacity);
 	}
+	
+	/*
+	 * WHERE
+	 */
 
 	@Override
 	public UpdatableCollection<E> where(Predicate<E> predicate) {
 		Util.removeUnsatisfyingElements(this, predicate);
 		return this;
 	}
+	
+	/*
+	 * SET
+	 */
 
 	@Override
 	public int set(Updater<E> updater) {
 		for (E element : this) {
 			updater.update(element);
+		}
+		return this.size();
+	}
+
+	@Override
+	public int set(Updater<E> updater1, Updater<E> updater2) {
+		@SuppressWarnings("unchecked")
+		List<Updater<E>> updaters = Arrays.asList(updater1, updater2);
+		set(updaters);
+		return this.size();
+	}
+
+	@Override
+	public int set(Updater<E> updater1, Updater<E> updater2, Updater<E> updater3) {
+		@SuppressWarnings("unchecked")
+		List<Updater<E>> updaters = Arrays.asList(updater1, updater2, updater3);
+		set(updaters);
+		return this.size();
+	}
+
+	@Override
+	public int set(Updater<E> updater1, Updater<E> updater2, Updater<E> updater3, Updater<E> updater4) {
+		@SuppressWarnings("unchecked")
+		List<Updater<E>> updaters = Arrays.asList(updater1, updater2, updater3, updater4);
+		set(updaters);
+		return this.size();
+	}
+
+	@Override
+	public int set(Updater<E> updater1, Updater<E> updater2, Updater<E> updater3, Updater<E> updater4, Updater<E> updater5) {
+		@SuppressWarnings("unchecked")
+		List<Updater<E>> updaters = Arrays.asList(updater1, updater2, updater3, updater4, updater5);
+		set(updaters);
+		return this.size();
+	}
+
+	@Override
+	public int set(Collection<Updater<E>> updaters) {
+		for (E element : this) {
+			for (Updater<E> updater : updaters) {
+				updater.update(element);
+			}
 		}
 		return this.size();
 	}
